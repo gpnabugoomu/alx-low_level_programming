0ffd7a446c31689f74648e132dc0adbf396b39a4
gpnabugoomu <gpnabugoomu@gmail.com>
2022-10-25 12:56:22 -0700

    new

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the larget of 3 integers
+ * Return: 0
+ */
+ int main(void)
+{
+	int d, e, f;
+	int largest;
+
+	d = 972;
+	e = -98;
+	f = 0;
+
+	largest = largest_number(d, e, f);
+
+	printf("%d is the largest number\n", largest);
+
+	return (0);
+}
