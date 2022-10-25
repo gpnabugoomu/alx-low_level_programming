1d1da1f360097b6e555d67013e4e6932b9c07a92
gpnabugoomu <gpnabugoomu@gmail.com>
2022-10-24 23:21:06 -0700

    changed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
-#include "main.h"
-
+#include "main.h" 
 /**
- * reset_to_98 - updates the value *n points to.
- * @n: the number to modify.
+ * reset_to_98 - Set value of a variable to 98 using a pointer
+ * @n: The address of the value needed to change
+ *
+ * Return: nothing
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
