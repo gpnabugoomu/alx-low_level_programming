197c55a899217b505c9bedd27dbed9d2d57cd056
Nabugoomu Gideon Philemon <83885533+gpnabugoomu@users.noreply.github.com>
2022-10-25 18:22:08 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+/**
+ * main-entry point
+ *
+ * Description: prints all possible combinations of two two-digit numbers
+ *
+ * Return:0 success, non zero fail
+ */
+
+int main(void)
+{
+  int a, n;
+
+  for (a = 0; a < 99; a++)
+	{
+	  for (n = a + 1; n <= 99; n++)
+		{
+			putchar('0' + a / 10);
+			putchar('0' + a % 10);
+
+			putchar(' ');
+
+			putchar('0' + n / 10);
+			putchar('0' + n % 10);
+
+			if (a == 98 && n == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
