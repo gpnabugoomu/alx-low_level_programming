2d372c3df1f77659d12f4430d8ccb4a55e3cf42f
Nabugoomu Gideon Philemon <83885533+gpnabugoomu@users.noreply.github.com>
2022-10-28 23:22:15 +0300

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,41 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
- * @a: an array of integers
- * @n: the number of elements to swap
+ * reverse_array - Reverse contents of array
+ * @a: The array to reverse
+ * @n: The size of the array
  *
- * Return: nothing.
+ * Return: Nothing
  */
-void print_array(int *a, int n)
+void reverse_array(int *a, int n)
 {
-	int i;
+int i, c;
 
-	i = 0;
-	while (i < n)
+for (i = 0; (i < (n - 1) / 2); i++)
 	{
-		if (i != 0)
-		{
-			printf(", ");
-		}
-		printf("%d", a[i]);
-		i++;
+	c = a[i];
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = c;
 	}
-	printf("\n");
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-	print_array(a, sizeof(a) / sizeof(int));
-	reverse_array(a, sizeof(a) / sizeof(int));
-	print_array(a, sizeof(a) / sizeof(int));
-	return (0);
 }
