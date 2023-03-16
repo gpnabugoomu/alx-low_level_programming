03d299dff1bcfe8af784a877a4c383ac565af43e
Nabugoomu Gideon Philemon <83885533+gpnabugoomu@users.noreply.github.com>
2023-03-16 19:43:47 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main : write a c program that prints to the standard error.
- *
- * Return: and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - write to stdout
+ * Return: Always 0
  */
-int main(void){
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
-	return(1);
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
