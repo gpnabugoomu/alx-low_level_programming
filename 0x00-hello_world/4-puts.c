d9403050a3f03d2b09aadc5a51654c0ecc2679f2
gpnabugoomu <gpnabugoomu@gmail.com>
2023-03-16 10:24:33 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * Description: prints a string to stdout.
+ * Return: Always 0 if success.
  */
-int main(void) 
-{
+int main(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
