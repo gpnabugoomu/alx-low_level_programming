9d19505311fc087887be7bfc5d3baa09c4a67b51
Nabugoomu Gideon Philemon <83885533+gpnabugoomu@users.noreply.github.com>
2023-03-18 15:30:50 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- * main-entry point
- *
- * Description: prints all possible combinations of two two-digit numbers
- *
- * Return:0 success, non zero fail
+ * main - Entry point
+ * Description:  A program that prints all possible combinations of two digits.
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
-  int a, n;
-  for (a = 0; a < 99; a++)
-	{
-	  for (n = a + 1; n <= 99; n++)
-		{
-			putchar('0' + a / 10);
-			putchar('0' + a % 10);
-
-			putchar(' ');
-
-			putchar('0' + n / 10);
-			putchar('0' + n % 10);
-
-			if (a == 98 && n == 99)
-				break;
-			putchar(',');
-			putchar(' ');
+int main(void){
+	int x = 0;
+	int y_z;
+	int s_z;
+	
+	int x2;
+	int y_z2;
+	int s_z2;
+	
+	while (c <= 98){
+		y_z = ( x / 10 + '0');
+		s_z = ( x % 10 + '0');
+		x2 = 0;
+		while (c2 <= 99){
+			y_z2 = (x2 / 10 + '0');
+			s_z2 = (x2 % 10 + '0');
+			
+			if (x < x2){
+				putchar(y_z);
+				putchar(s_z);
+				putchar(' ');
+				putchar(y_z2);
+				putchar(s_z2);
+				
+				if ( x != 98){
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			x2++;
 		}
+		x++;
 	}
 	putchar('\n');
 	return (0);
