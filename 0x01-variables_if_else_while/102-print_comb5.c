5108bd37f5dc13a6ba23f43907cba410572bef15
gpnabugoomu <gpnabugoomu@gmail.com>
2023-03-18 06:00:39 -0700

    last commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,30 @@
-
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	int a, b;
+	a = 0;
+	while (a <= 98){
+		b = a + 1;
+		while (b <= 99){
+			putchar(a / 10 + '0');
+			putchar(a % 10 + '0');
+			putchar(' ');
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+			if (a == 98 && b == 99){
+				putchar('\n');
+			} else{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	return (0);
+}
