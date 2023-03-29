e151e79668f1277d3a62eef8f5011a3c428ba084
gpnabugoomu <gpnabugoomu@gmail.com>
2023-03-28 22:37:04 -0700

    swap and string length

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,16 @@
 #include "main.h"
+
 /**
  * swap_int - swaps values of two integers
  * @a: integer to be swapped
  * @b: integer to be swapped
+ * Return: 0
  */
 void swap_int(int *a, int *b)
 {
-	int a;
+	int change;
+	change = *a;
+	*a = *b;
+	*b = change;
+}
 
