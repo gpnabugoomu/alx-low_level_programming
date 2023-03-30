f2f3011a29785a53a71df676611c24219d3cf2e2
gpnabugoomu <gpnabugoomu@gmail.com>
2023-03-30 14:43:25 -0700

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar (a + '0');
+		}
+		a++;
+	}
+
+	_putchar ('\n');
+}
