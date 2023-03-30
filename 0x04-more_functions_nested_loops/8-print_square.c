6dbcc7a4b11266a302955e7466861a15659cd5b1
gpnabugoomu <gpnabugoomu@gmail.com>
2023-03-30 14:33:04 -0700

    sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ *
+ * @size: print size
+ *
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+				_putchar (35);
+			_putchar ('\n');
+		}
+	}
+
+	else
+		_putchar ('\n');
+}
