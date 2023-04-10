138cd2243a420c58756288112a6485adcbd50323
gpnabugoomu <gpnabugoomu@gmail.com>
2023-04-10 16:06:28 -0700

    isupper

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -2,11 +2,10 @@
 
 /**
  * _isupper - checks for uppercase letters
- * @c: character to be checked
- * 
+ * @c: character to be checked 
  * Return: 1 if c is uppercase, 0 otherwise
 */
-int _isupper(char c)
+int _isupper(int c)
 {
-    return(c >= 'A' && c <= 'Z');
+	return (c >= 'A' && c <= 'Z');
 }
