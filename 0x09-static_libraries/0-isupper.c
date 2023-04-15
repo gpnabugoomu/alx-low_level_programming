c2a14c58609e02f43537018881ac945540389bfd
gpnabugoomu <gpnabugoomu@gmail.com>
2023-04-15 07:27:06 -0700

    file

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - upppercase letters
+ * @c: char to be checked
+ *
+ * Return: o or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+	return (0);
+}
