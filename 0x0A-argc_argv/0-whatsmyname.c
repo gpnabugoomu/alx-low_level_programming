dea522667f5fbc2ef2181f34929318ce96d9e76b
gpnabugoomu <gpnabugoomu.gmail.com>
2023-06-12 08:02:43 -0700

    whatsname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - function that pronts its name
+ * @argc: the argument count
+ * @argv: the argument vector
+ *
+ * Return: Always zero
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
