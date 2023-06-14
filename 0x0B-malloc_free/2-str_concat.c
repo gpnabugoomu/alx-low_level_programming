a58dc5d00610a71d8bc5b38c8be71993f7065756
gpnabugoomu <gpnabugoomu.gmail.com>
2023-06-14 03:10:56 -0700

    updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,38 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-/*
+/**
  * str_concat - function that concatenates two strings
  * @s1: the first string
  * @s2: the second string
+ *
  * Return: Always (0), NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
+	char *new;
 	int s1_len, s2_len, a, b;
-	char *str;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	else if (s2 == NULL)
-	{
-		s2 = "";
-	}
-	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-		continue;
-	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-		continue;
-	str = malloc(s1_len + s2_len + 1);
+	s1 == NULL ? s1 = "" : s1;
+	s2 == NULL ? s2 = "" : s2;
 
-	if (str == NULL)
-	{
+	for (s1_len = 0; s1[s1_len]; s1_len++)
+		;
+	for (s2_len = 0; s2[s2_len]; s2_len++)
+		;
+
+	new = malloc(sizeof(char) * (s1_len + s2_len + 1));
+	if (!new)
 		return (NULL);
-	}
-	a = 0;
 
-	while (a < s1_len)
-	{
-		str[a] = s1[a];
-		a++;
-	}
-	b = 0;
+	for (a = 0; a < s1_len; a++)
+		new[a] = s1[a];
 
-	while (a < (s1_len + s2_len))
-	{
-		str[a] = s2[b];
-		a++;
-		b++;
-	}
-	str[a] = '\0';
-	return (str);
+	for (b = 0; b < s2_len; b++)
+		new[a + b] = s2[b];
+
+	new[a + b] = '\0';
+
+	return (new);
 }
