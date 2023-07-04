35eb81d2cc339cfbf3b4590f8d4d3babd817ee01
gpnabugoomu <gpnabugoomu@gmail.com>
2023-07-04 07:11:15 -0700

    1 lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - function to return the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num_of_nodes = 0;
+
+	while (h)
+	{
+		h = h->next;
+		num_of_nodes++;
+	}
+
+	return (num_of_nodes);
+}
